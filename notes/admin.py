6d6d93e4cbc05b43f700761bc0f17bdfed5677f5
Mark Christopher Lumbay <lumbaymark@gmail.com>
2025-08-03 17:04:02 +0800

    added note admin display and filters

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Note
 
+class NoteAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'description', 'prompt', 'file_path', 'created_at', 'updated_at')
+  list_filter = ('name', 'created_at')
+
 # Register your models here.
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
