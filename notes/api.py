51992d3c4c7660041e2c4816481b78aa161fb8df
Mark Christopher Lumbay <lumbaymark@gmail.com>
2025-08-03 18:00:25 +0800

    implemented markdown extraction

--- notes/api.py
+++ notes/api.py
@@ -1,7 +1,9 @@
+from cmd import PROMPT
 from ninja import NinjaAPI
 from .models import Note
 from pydantic import BaseModel
 from typing import Optional, List
+import markdown
 
 api = NinjaAPI()
 
@@ -10,6 +12,14 @@ class NoteBody(BaseModel):
   description: Optional[str] = None
   prompt: str
   file_path: str
+
+class AnalyzeNoteBody(BaseModel):
+  note_id: int
+  markdown: str
+
+class AnalyzeNoteResponse(BaseModel):
+  section_name: str
+  section_content: str
 
 class Error(BaseModel):
   message: str
@@ -52,3 +62,47 @@ def get_notes(request):
     return 500, {"message": str(e)}
   except Note.DoesNotExist:
     return 404, {"message": "Note not found"}
+
+@api.post("/analyze_note", response={200: AnalyzeNoteResponse, 500: Error})
+def analyze_note(request, body: AnalyzeNoteBody):
+
+  # Get the note name and its prompt
+  try:
+    note = Note.objects.get(id=body.note_id)
+  except Exception as e:
+    return 500, {"message": str(e)}
+
+  noteFilePath = note.file_path
+  notePrompt = note.prompt
+
+  markdown_section_level = body.markdown.count("#")
+  markdown_section = body.markdown
+
+  in_markdown_section = False
+  section_content = ""
+  section_name = ""
+
+  # Open and extract data from the file
+  with open(noteFilePath, "r") as file:
+    for line in file:
+      line_stripped = line.strip()
+
+      if line_stripped.startswith("#"):
+        if line_stripped == markdown_section:
+          in_markdown_section = True
+          section_name = line_stripped.replace("#", "").strip()
+          continue
+
+      if in_markdown_section:
+        section_content += line
+
+      if line_stripped.startswith("#") and line_stripped.count("#") < markdown_section_level:
+        break
+
+  return AnalyzeNoteResponse(
+    section_name=section_name,
+    section_content=section_content
+  )
+
+
+
