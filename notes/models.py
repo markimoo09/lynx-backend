b1b4322a718c05dcd165629d47ce5950eac29bd1
Mark Christopher Lumbay <lumbaymark@gmail.com>
2025-08-03 16:17:30 +0800

    created notes model and added notes to app dir

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  prompt = models.TextField()
+  file_path = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
